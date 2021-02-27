be6f1064f7e62dbe1adf181b06f73cc97d5799ab
Jewel Carrier-Davis <jujicb@gmail.com>
2021-02-27 00:54:44 -0500

    commit tree 90a88e40b25a918513f0f1ccd8368e579be2ba9c

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
-  # code here
+
+def my_each(array) # put argument(s) here
+  arr_length = 0
+  i = 0
+
+  if arr_length < array.length
+    while arr_length < array.length
+      yield array[i]
+      i = i + 1
+      arr_length = arr_length + 1
+    end
+    array
+  elsif arr_length == array.length
+    array
+  elsif arr_length == 0
+    return "This block should not run!"
+  end
 end
